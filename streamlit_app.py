fa65f5908473766152dcb37bcd47c2a9c6797e7f
t-cm <110851648+t-cm@users.noreply.github.com>
2024-08-03 01:02:45 +0000

    revert

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,116 +44,116 @@ def get_data():
 # Short All
 '''
 
-'''
----
-Attempts to uncover patterns of rise and fall in the market have produced a colorful history...
+# '''
+# ---
+# Attempts to uncover patterns of rise and fall in the market have produced a colorful history...
 
-&nbsp;
-'''
-get_images()
+# &nbsp;
+# '''
+# get_images()
 
-'''
----
-It's inductively sound and often accurate to think that the general trends of the past will continue. Going long stocks and bonds at risk parity has been an efficacious strategy.
+# '''
+# ---
+# It's inductively sound and often accurate to think that the general trends of the past will continue. Going long stocks and bonds at risk parity has been an efficacious strategy.
 
-On the other hand, betting on a broad decline in asset prices (in the United States) is a bet that things will not merely stay the same.
+# On the other hand, betting on a broad decline in asset prices (in the United States) is a bet that things will not merely stay the same.
 
-In this document, we'll explore the 'short-everything' strategy—a portfolio weighted between short positions on the S&P 500, 10-year Treasuries, and leveraged Dollar Index (DXY). The analysis will cover:
+# In this document, we'll explore the 'short-everything' strategy—a portfolio weighted between short positions on the S&P 500, 10-year Treasuries, and leveraged Dollar Index (DXY). The analysis will cover:
 
-1. **Asset Overview**: How have stocks, bonds, DXY, and cash have performed since approximately 1971? How do we measure performance?
-2. **Portfolio Construction**: How do we size the bet? How we'll has it worked in the past?
-3. **Historical Reconstruction**: When it worked...why did it work?
-4. **Prediction**: How bad of an idea is it to put on this trade in 2024H2? What things might I believe that would make this a good trade?
----
+# 1. **Asset Overview**: How have stocks, bonds, DXY, and cash have performed since approximately 1971? How do we measure performance?
+# 2. **Portfolio Construction**: How do we size the bet? How we'll has it worked in the past?
+# 3. **Historical Reconstruction**: When it worked...why did it work?
+# 4. **Prediction**: How bad of an idea is it to put on this trade in 2024H2? What things might I believe that would make this a good trade?
+# ---
 
-'''
+# '''
 
-## PART 1
+# ## PART 1
 
-'''
-How have the following three investment apporaches fared historically?
+# '''
+# How have the following three investment apporaches fared historically?
 
-1. **Cash**: Earn the "risk-free" rate
-2. **Long**: long some weighting of stocks and bonds
-3. **Short**: short some weighting of stocks, bonds, and leverged USD 
+# 1. **Cash**: Earn the "risk-free" rate
+# 2. **Long**: long some weighting of stocks and bonds
+# 3. **Short**: short some weighting of stocks, bonds, and leverged USD 
 
-Cash is the effective fedral funds rate (FFR). Stocks are SPX total returns. USD is DXY. Bonds are 10Y treasuryies.
+# Cash is the effective fedral funds rate (FFR). Stocks are SPX total returns. USD is DXY. Bonds are 10Y treasuryies.
 
-Before we investiagte the portfolio performance let's look at how indivdual assets perform. You can use the interactive graph to explore the rolling, anualized excess returns of each asset over different windows. Note that throughout this document we're going to focus mostly on returns on or after 1971 as I think the ending of Bretton Woods marks a fundamental shift in the monetary system.  
+# Before we investiagte the portfolio performance let's look at how indivdual assets perform. You can use the interactive graph to explore the rolling, anualized excess returns of each asset over different windows. Note that throughout this document we're going to focus mostly on returns on or after 1971 as I think the ending of Bretton Woods marks a fundamental shift in the monetary system.  
 
----
+# ---
 
-'''
+# '''
 
-df = get_data()  
-fig_er = plot_rolling_excess_returns(df)
-st.plotly_chart(fig_er)
-
-
-
-'''
-Let's also look at the yields. Periods where the risk free rate exceedes the 10Y yeild - shaded in red - bode porly for a long only strategy (presumably because if treaury buyers are willing to accept lower long-term yields it means they can't figure out a better place to park capital). 
-
----
-''' 
-
-df = get_data()  
-fig_yeild = plot_yield_comparison(df)
-st.plotly_chart(fig_yeild)
+# df = get_data()  
+# fig_er = plot_rolling_excess_returns(df)
+# st.plotly_chart(fig_er)
 
 
-'''
-On the data side, FFR is pulled directly FRED. SPX is adjusted to include dividends. 10Y bond prices are approximated by calculating it's modified duration and change in yield. After data wrangling we end up with the daily absolute and excess returns for each asset. 
-When calculating returns, the short positions in stocks and bonds are assumed to be 100% short (i.e., the exact inverse of a long position) without any additional leverage. Bonds are always rolled to keep constant 10y meaturies. Borrowing cost, exchange fees, and slippage are assumed to always be de-minimis. 
 
-Finally, before we look construct our portfolio, we can get a sense of stock bond correlation by looking at the performance of stocks and bonds per year...this is just an explorarty graph but it would appear that the correlation between stocks and bonds is pretty weak. 
+# '''
+# Let's also look at the yields. Periods where the risk free rate exceedes the 10Y yeild - shaded in red - bode porly for a long only strategy (presumably because if treaury buyers are willing to accept lower long-term yields it means they can't figure out a better place to park capital). 
 
----
+# ---
+# ''' 
 
-'''
-
-df = get_data()  
-fig_yeild = plot_stock_bond_correlation(df)
-st.plotly_chart(fig_yeild)
+# df = get_data()  
+# fig_yeild = plot_yield_comparison(df)
+# st.plotly_chart(fig_yeild)
 
 
-'''
-These are the returns of the risk parity short portfolio by decade. The bubble size corresponds to how much better the short RP portfolio performed vs the long portfolio. The muted dots, mean that the naieve long RP portfolio outperformed.
+# '''
+# On the data side, FFR is pulled directly FRED. SPX is adjusted to include dividends. 10Y bond prices are approximated by calculating it's modified duration and change in yield. After data wrangling we end up with the daily absolute and excess returns for each asset. 
+# When calculating returns, the short positions in stocks and bonds are assumed to be 100% short (i.e., the exact inverse of a long position) without any additional leverage. Bonds are always rolled to keep constant 10y meaturies. Borrowing cost, exchange fees, and slippage are assumed to always be de-minimis. 
 
----
-'''
+# Finally, before we look construct our portfolio, we can get a sense of stock bond correlation by looking at the performance of stocks and bonds per year...this is just an explorarty graph but it would appear that the correlation between stocks and bonds is pretty weak. 
 
-df = get_data()  
-fig_yeild = plot_portfolio_returns_bubble_year(df)
-st.plotly_chart(fig_yeild)
+# ---
 
-'''
+# '''
 
-To reiterate we want to find the best portfolio among:
-
-1. **Cash**: Earn the "risk-free" rate
-2. **Long**: long some weighting of stocks and bonds
-3. **Short**: short some weighting of stocks, bonds, and leverged USD 
-
-To build a short-only porfolio we will choose (w_spx, w_10y, w_dxy) with constraints (wi in [0,-1]), sum(wi=-1). Further, because we are using a 5x levered DXY index, we will restrict w_dxy to a max of 1/5. This means the portfolio can have up between (0,-1) exposure to the dollar. This also means that in the short only strategy we are short 80%+ of our capital base in some combination of stocks and bonds
-
-To build a long-only portfolio we will chose (w_spx, w_10y)  (wi in [0,1]), sum(wi=1). In the long portfolio we are alredady implicitly long dollar (the assets are denominated in dollar and the value of the stocks and bonds are the expectation of future  so I don't see a need to bet on the DYX. Without DYX the long portfolios we make will be somewhat similar to familar 60/40 strategies and is therefore easy to interpret.
+# df = get_data()  
+# fig_yeild = plot_stock_bond_correlation(df)
+# st.plotly_chart(fig_yeild)
 
 
-The **best** portfolio is the one with the maximum sharpe ratio over the window of time we deem relevant. 
+# '''
+# These are the returns of the risk parity short portfolio by decade. The bubble size corresponds to how much better the short RP portfolio performed vs the long portfolio. The muted dots, mean that the naieve long RP portfolio outperformed.
 
-We can simplify this by problem by thinking about with considering the excess returns of each portfolio. 
+# ---
+# '''
 
----
+# df = get_data()  
+# fig_yeild = plot_portfolio_returns_bubble_year(df)
+# st.plotly_chart(fig_yeild)
 
-'''
+# '''
+
+# To reiterate we want to find the best portfolio among:
+
+# 1. **Cash**: Earn the "risk-free" rate
+# 2. **Long**: long some weighting of stocks and bonds
+# 3. **Short**: short some weighting of stocks, bonds, and leverged USD 
+
+# To build a short-only porfolio we will choose (w_spx, w_10y, w_dxy) with constraints (wi in [0,-1]), sum(wi=-1). Further, because we are using a 5x levered DXY index, we will restrict w_dxy to a max of 1/5. This means the portfolio can have up between (0,-1) exposure to the dollar. This also means that in the short only strategy we are short 80%+ of our capital base in some combination of stocks and bonds
+
+# To build a long-only portfolio we will chose (w_spx, w_10y)  (wi in [0,1]), sum(wi=1). In the long portfolio we are alredady implicitly long dollar (the assets are denominated in dollar and the value of the stocks and bonds are the expectation of future  so I don't see a need to bet on the DYX. Without DYX the long portfolios we make will be somewhat similar to familar 60/40 strategies and is therefore easy to interpret.
 
 
-st.markdown(r"""
-For both the long and the short portfolios we find the covarience matrix, and the weights. Use a trailing window of 252 trading days ~1 year. And compute the predicted best weights daily. The weights are forward looking, meaning we calculate them without knowing what the realized vol is, Assume that we can rebalance the portfolios once per day.
+# The **best** portfolio is the one with the maximum sharpe ratio over the window of time we deem relevant. 
 
-Risk parity gives us a realistic benchamrk for what a good, but non-propeitary porfolio would look like. 
+# We can simplify this by problem by thinking about with considering the excess returns of each portfolio. 
+
+# ---
+
+# '''
+
+
+# st.markdown(r"""
+# For both the long and the short portfolios we find the covarience matrix, and the weights. Use a trailing window of 252 trading days ~1 year. And compute the predicted best weights daily. The weights are forward looking, meaning we calculate them without knowing what the realized vol is, Assume that we can rebalance the portfolios once per day.
+
+# Risk parity gives us a realistic benchamrk for what a good, but non-propeitary porfolio would look like. 
             
-A big reason risk parity makes a "good" portfolio is that volatility has autoregressive structure It's outside of the scope of this doc, but we could improve upon the naive implemntation. By having better predictive models for covarience matrix like a GARCH model.
+# A big reason risk parity makes a "good" portfolio is that volatility has autoregressive structure It's outside of the scope of this doc, but we could improve upon the naive implemntation. By having better predictive models for covarience matrix like a GARCH model.
 
-""")
+# """)
